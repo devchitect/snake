7734a56c92306d27e3167ea27dc113d443bb316d
Dzung <dzungko@gmail.com>
2022-12-15 10:28:27 +0700

    update golden-apple

--- snake.js
+++ snake.js
@@ -12,6 +12,7 @@ window.onload = function (){
     const pythonColor = "lime";
     const pythonBorder = "black";
     const appleColor= "red";
+    const gappleColor = "yellow";
     const unitSize = 20;
 
     let running = false;
@@ -23,9 +24,9 @@ window.onload = function (){
 
     let portal1X,portal2X;
     let portal1Y,portal2Y;
-    let portals = [];
+    // let portals = [];
 
-    let gameSpeed = 85;
+    let gameSpeed = 111;
 
     let python =[
         {x:unitSize*2   ,y:unitSize*10},
@@ -34,6 +35,7 @@ window.onload = function (){
 
     window.addEventListener("keyup",changeDirection);
     resetBtn.addEventListener("click",resetGame);
+    window.addEventListener("keyup",resetGame);
 
     gameStart();
     document.getElementById("slow").addEventListener("click",slow);
@@ -47,24 +49,29 @@ window.onload = function (){
         running = true;
 
         createPortal();
-        createApple();
+        locateApple();
+        locateGapple();
         drawApple();
         nextTick();
 
     }
+
     function drawScore(){
         context.font = "16.5px Orbitron-M";
         context.fillStyle = "rgba(255, 255, 255)";
         context.fillText("Score:  ", unitSize,30 );
         context.fillStyle = "lime";
         context.fillText("  " + score, unitSize*4,30 );
-            if(score === 15){gameSpeed=70}
-            if(score === 25){gameSpeed=63}
-            if(score === 35){gameSpeed=57}
-            if(score === 70){gameSpeed=50}
-            if(score === 90){gameSpeed=45}
-            if(score === 120){gameSpeed=35}
-            console.log(gameSpeed);
+            if(score === 10 || score > 10){gameSpeed=100}
+            if(score === 25 || score > 25){gameSpeed=90}
+            if(score === 35 || score > 35){gameSpeed=80}
+            if(score === 70 || score > 70){gameSpeed=70}
+            if(score === 90 || score >90){gameSpeed=60}
+            if(score === 105 || score > 105){gameSpeed=50}
+            if(score === 120 || score > 120){gameSpeed=40}
+            if(score === 135 || score > 135){gameSpeed=35}
+
+        console.log(gameSpeed);
     }
 
     function nextTick(){
@@ -74,10 +81,14 @@ window.onload = function (){
                 createPortal();
                 drawScore();
                 drawApple();
+                if(score % 5 === 0){
+                    drawGapple();
+                }
                 movePython();
                 drawPython();
                 checkGameOver();
                 nextTick();
+
             },gameSpeed);
         }
         else {gameOver()}
@@ -88,29 +99,48 @@ window.onload = function (){
         context.fillRect(0,0,width,height);
     }
 
-    function createApple(){
+    function locateApple(){
         function randomApple(num){
             return Math.round((Math.random() * (num) / unitSize)) * unitSize;
         }
         appleX = randomApple(width-unitSize);
         appleY = randomApple(height-unitSize);
+
+    }
+    function locateGapple(){
+        function randomGapple(num){
+            return Math.round((Math.random() * (num) / unitSize)) * unitSize;
+        }
+
+
+        gappleX = randomGapple(width-unitSize);
+        gappleY = randomGapple(height-unitSize);
     }
     function drawApple(){
         context.fillStyle = appleColor;
         context.fillRect(appleX,appleY,unitSize,unitSize);
     }
+    function drawGapple(){
+        context.fillStyle = gappleColor;
+        context.fillRect(gappleX,gappleY,unitSize,unitSize);
+    }
 
     function movePython(){
         const head = {x:python[0].x + xVelocity,
                       y:python[0].y + yVelocity};
-        python.unshift(head);
+
+            python.unshift(head);
         //eat apple
         if(python[0].x === appleX && python[0].y === appleY){
+            score+= 1;
+            locateApple();
+        }else if(python[0].x === gappleX && python[0].y === gappleY){
             score+= 3;
-            createApple();
+            locateGapple();
         }else {
             python.pop();
         }
+
     }
     function drawPython(){
         context.fillStyle= pythonColor;
@@ -119,6 +149,7 @@ window.onload = function (){
             context.fillRect(part.x,part.y,unitSize,unitSize);
             context.strokeRect(part.x,part.y,unitSize,unitSize);
         })
+
     }
     function changeDirection(event){
         const keyPressed = event.keyCode;
@@ -191,6 +222,7 @@ window.onload = function (){
     function throughPortal() {
 
     }
+
     function gameOver(){
         document.getElementById("gameOver").style.visibility = "visible";
         document.getElementById("score").innerText = score;
@@ -201,8 +233,13 @@ window.onload = function (){
             xVelocity = 0;
             yVelocity = 0;
 
+            function randomNum(num){
+                return Math.round((Math.random() * (num) / unitSize)) * unitSize;
+            }
+
+
             python =[
-                {x:unitSize*2   ,y:unitSize*15},
+                {x:randomNum(width-unitSize),y:randomNum(height-unitSize)},
                 // {x:unitSize     ,y:unitSize*15},
             ];
 
@@ -211,6 +248,7 @@ window.onload = function (){
             document.getElementById("gameOver").style.visibility = "hidden";
         }
     }
+
 
 
 }
